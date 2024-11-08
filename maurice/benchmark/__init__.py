4da58ab6aa6794d99a0df934e48c8331bcebde30
Augustin Godinot <agodinot@hotmail.fr>
2024-11-08 12:34:35 +0100

    add bench description in `get_benchmark`

--- maurice/benchmark/__init__.py
+++ maurice/benchmark/__init__.py
@@ -6,7 +6,11 @@ from .model_reuse import ModelReuse
 
 
 def get_benchmark(
-    name: str, data_dir: Path, models_dir: Path, device: str
+    name: str,
+    data_dir: Path,
+    models_dir: Path,
+    device: str,
+    with_description: bool = False,
 ) -> Benchmark:
     match name:
         case "SACBenchmark":
@@ -15,6 +19,7 @@ def get_benchmark(
                 models_dir=models_dir,
                 device=device,
             )
+            desc = "Original paper: Are You Stealing My Model? Sample Correlation for Fingerprinting Deep Neural Networks"
 
         case "ModelReuse":
             bench = ModelReuse(
@@ -22,8 +27,12 @@ def get_benchmark(
                 models_dir=models_dir,
                 device=device,
             )
+            desc = "Original paper: ModelDiff: Testing-Based DNN Similarity Comparison for Model Reuse Detection"
 
         case _:
             raise NotImplementedError()
+
+    if with_description:
+        return bench, desc
 
     return bench
